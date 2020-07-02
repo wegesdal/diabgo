08b2c3c1fdc16b38302e577242d39b264ba88dfb
William Egesdal <wegesdal@Williams-MacBook.local>
2020-07-01 21:25:45 -1000

    animated player sprite

--- main.go
+++ main.go
@@ -15,35 +15,19 @@ import (
 
 const (
 	windowWidth  = 800
-	windowHeight = 800
+	windowHeight = 600
 	// sprite tiles are squared, 64x64 size
 	tileSize = 64
 	f        = 0 // floor identifier
 	w        = 1 // wall identifier
 )
 
-var levelData = [][]uint{
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // This row will be rendered in the lower left part of the screen (closer to the viewer)
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // And this in the upper right
-}
+var levelData = [32][32]uint8{}
 
 var win *pixelgl.Window
 var floorTile, wallTile *pixel.Sprite
 var tiles []*pixel.Sprite
+var playerFrames []*pixel.Sprite
 
 var (
 	camPos       = pixel.ZV
@@ -84,23 +68,68 @@ func run() {
 		panic(err)
 	}
 
-	batch := pixel.NewBatch(&pixel.TrianglesData{}, pic)
+	mapBatch := pixel.NewBatch(&pixel.TrianglesData{}, pic)
 
 	tiles = append(tiles, pixel.NewSprite(pic, pixel.R(0, 128, tileSize, 192)))
 	tiles = append(tiles, pixel.NewSprite(pic, pixel.R(0, 448, tileSize, 512)))
 
+	// load in player sprites
+
+	playerSprites, err := loadPicture("CitizenSheet.png")
+	if err != nil {
+		panic(err)
+	}
+
+	playerBatch := pixel.NewBatch(&pixel.TrianglesData{}, playerSprites)
+
+	// wizard
+	//playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(0, 0, 24, 48)))
+
+	// walking down
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*4, 52*3, 22*5, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*5, 52*3, 22*6, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*6, 52*3, 22*7, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*7, 52*3, 22*8, 52*4)))
+
+	// walking up
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*8, 52*3, 22*9, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*9, 52*3, 22*10, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*10, 52*3, 22*11, 52*4)))
+	playerFrames = append(playerFrames, pixel.NewSprite(playerSprites, pixel.R(22*11, 52*3, 22*12, 52*4)))
+
 	var (
-		frames = 0
-		second = time.Tick(time.Second)
+		frames       = 0
+		ticks        = 0.0
+		currentFrame = 0
+		second       = time.Tick(time.Second)
 	)
 
 	last := time.Now()
 
-	updateBatch(batch)
+	const (
+		NW = iota - 2
+		SW
+		IDLE
+		SE
+		NE
+	)
+
+	updateMap(mapBatch)
+
+	updatePlayer(playerBatch, 0, SW)
 
 	for !win.Closed() {
 
 		dt := time.Since(last).Seconds()
+
+		ticks += dt
+
+		if ticks > 0.3 {
+			currentFrame++
+			updatePlayer(playerBatch, currentFrame%4, SE)
+			ticks = 0
+		}
+
 		last = time.Now()
 
 		cam := pixel.IM.Scaled(camPos, camZoom).Moved(win.Bounds().Center().Sub(camPos))
@@ -120,7 +149,7 @@ func run() {
 				}
 
 			}
-			updateBatch(batch)
+			updateMap(mapBatch)
 		}
 
 		if win.Pressed(pixelgl.KeyLeft) {
@@ -135,10 +164,12 @@ func run() {
 		if win.Pressed(pixelgl.KeyUp) {
 			camPos.Y += camSpeed * dt
 		}
+
 		// camZoom *= math.Pow(camZoomSpeed, win.MouseScroll().Y)
 
 		win.Clear(colornames.Darkmagenta)
-		batch.Draw(win)
+		mapBatch.Draw(win)
+		playerBatch.Draw(win)
 		win.Update()
 
 		frames++
@@ -152,7 +183,20 @@ func run() {
 	}
 }
 
-func updateBatch(batch *pixel.Batch) {
+func updatePlayer(batch *pixel.Batch, frame int, direction int) {
+	batch.Clear()
+	isoCoords := cartesianToIso(pixel.V(float64(0), float64(0)))
+	mat := pixel.IM.Moved(isoCoords)
+
+	if direction < 0 {
+		mat = mat.ScaledXY(pixel.ZV, pixel.V(-1, 1))
+	}
+
+	var startFrame = (direction*direction - 1) * 2
+	playerFrames[frame+startFrame].Draw(batch, mat)
+}
+
+func updateMap(batch *pixel.Batch) {
 
 	batch.Clear()
 	for x := len(levelData) - 1; x >= 0; x-- {
@@ -164,7 +208,6 @@ func updateBatch(batch *pixel.Batch) {
 			tiles[levelData[x][y]].Draw(batch, mat)
 		}
 	}
-
 }
 
 func cartesianToIso(pt pixel.Vec) pixel.Vec {
